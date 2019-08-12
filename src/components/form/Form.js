b5809802196c0e404a4174de58dee1c38dec9c80
Alan Giojelli <alangiojelli@Alans-MacBook-Air.local>
2019-08-12 14:08:58 -0300

    Fixing styles

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -6,44 +6,42 @@ import TextField from '@material-ui/core/TextField';
 const Form = () => (
 	<Fragment>
 		<h2>Patient Information</h2>
-		<div className="container">
-			<Grid container spacing={2}>
-				<Grid item xs={6} md={4}>
-					<TextField
-						required
-						id="standard-name"
-						label="Name"
-						margin="normal"
-						InputLabelProps={{
-							shrink: true
-						}}
-					/>
-				</Grid>
-				<Grid item xs={6} md={4}>
-					<TextField
-						required
-						id="standard-name"
-						label="Address"
-						margin="normal"
-						InputLabelProps={{
-							shrink: true
-						}}
-					/>
-				</Grid>
-				<Grid item xs={6} md={4}>
-					<TextField
-						id="date"
-						label="Birthday"
-						type="date"
-						margin="normal"
-						defaultValue="2017-05-24"
-						InputLabelProps={{
-							shrink: true
-						}}
-					/>
-				</Grid>
+		<Grid container spacing={2}>
+			<Grid item xs={6} md={4}>
+				<TextField
+					required
+					id="standard-name"
+					label="Name"
+					margin="normal"
+					InputLabelProps={{
+						shrink: true
+					}}
+				/>
 			</Grid>
-		</div>
+			<Grid item xs={6} md={4}>
+				<TextField
+					required
+					id="standard-name"
+					label="Address"
+					margin="normal"
+					InputLabelProps={{
+						shrink: true
+					}}
+				/>
+			</Grid>
+			<Grid item xs={6} md={4}>
+				<TextField
+					id="date"
+					label="Birthday"
+					type="date"
+					margin="normal"
+					defaultValue="2017-05-24"
+					InputLabelProps={{
+						shrink: true
+					}}
+				/>
+			</Grid>
+		</Grid>
 	</Fragment>
 );
 
